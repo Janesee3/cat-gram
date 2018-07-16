80e09a4bc9054371f5a5817bde6ae5d8178e16fe
See Loo Jane <muigek34@yahoo.com.sg>
2018-07-16 15:59:38 +0800

    add allow cross origin

--- app.js
+++ app.js
@@ -35,6 +35,14 @@ db.on("error", error => {
 const app = express();
 
 app.use(express.json());
+app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header(
+		"Access-Control-Allow-Headers",
+		"Origin, X-Requested-With, Content-Type, Accept"
+	);
+	next();
+});
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.get("/", (req, res, next) => {
 	res.json("hello");
