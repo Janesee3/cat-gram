b9d33de1016f92668a80ac61726e3b9f8b85f57b
See Loo Jane <muigek34@yahoo.com.sg>
2018-07-16 11:57:27 +0800

    Add connection to mongoose in app js (should possibly move to server.js ?)

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const swaggerUi = require("swagger-ui-express");
 const swaggerDocument = require("./swagger.json");
 
+// Open DB connection
+mongoose.connect("mongodb://localhost/cat-gram");
+const db = mongoose.connection;
+db.on("error", error => {
+	console.error("An error occurred!", error);
+});
+
 const app = express();
+
 app.use(express.json());
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.get("/", (req, res, next) => {
 	res.json("hello");
 });
-  
-
 
 module.exports = app;
