7c5bbf3b664d1e7d1a5d445ad2d2d388ad62ae1d
See Loo Jane <muigek34@yahoo.com.sg>
2018-07-16 14:58:19 +0800

    Add validator for author field during Post creation

--- models/Post.js
+++ models/Post.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const User = require('./User');
 
 // Create schema
 const postSchema = mongoose.Schema(
@@ -6,13 +7,13 @@ const postSchema = mongoose.Schema(
 		author: {
 			type: mongoose.Schema.Types.ObjectId,
 			ref: "User", // String must be the same as the string in Author model!
-			required: [true, "User ID is required!"]
-			// validate: {
-			// 	validator: user => {
-			// 		return User.findById(user); // returns false if cant find such author with this id
-			// 	},
-			// 	message: "Cannot find user with this ID."
-			// }
+			required: [true, "User ID is required!"],
+			validate: {
+				validator: user => {
+					return User.findById(user); // returns false if cant find such author with this id
+				},
+				message: "Cannot find user with this ID."
+			}
 		},
 		caption: { type: String, required: [true, "Caption is required!"] },
 		image: { type: String, required: [true, "Image url is required!"] },
