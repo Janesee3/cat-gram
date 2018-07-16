d3b8cba59b3092c24cd801d86ff2a19f70a62347
See Loo Jane <muigek34@yahoo.com.sg>
2018-07-16 13:51:38 +0800

    Add dummy route to post-router

--- routes/post-router.js
+++ routes/post-router.js
@@ -1,3 +1,14 @@
 const express = require("express");
 const router = express.Router();
+const Post = require('../models/Post');
+const mongoose = require('mongoose');
 
+router.use(express.json());
+
+router.get('/', (req, res, next) => {
+    res.json("Hi from post router.")
+})
+
+module.exports = (app) => {
+    app.use('/posts', router)
+}
