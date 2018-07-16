4c209fd2510b98ce9d26c99c529d621e524f786b
See Loo Jane <muigek34@yahoo.com.sg>
2018-07-16 10:59:12 +0800

    Link up swagger docs route

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
 
 const app = express();
 app.use(express.json());
 
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.get("/", (req, res, next) => {
 	res.json("hello");
 });
