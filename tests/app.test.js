5d7731300af360dd0b9aa7da0650b0362579b29d
See Loo Jane <muigek34@yahoo.com.sg>
2018-07-17 14:04:06 +0800

    Remove app.test

--- tests/app.test.js
+++
@@ -1,7 +0,0 @@
-const app = require("../app");
-const request = require("supertest");
-
-it("GET / should return dummy string in response body", async () => {
-	const response = await request(app).get("/");
-	expect(response.body).toBe("hello");
-});
