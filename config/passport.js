d8f43ee6ef430713a530708599f19656daeb578c
See Loo Jane <muigek34@yahoo.com.sg>
2018-07-17 11:10:21 +0800

    Add passport config

---
+++ config/passport.js
@@ -0,0 +1,30 @@
+const passport = require("passport");
+const passportJWT = require("passport-jwt");
+const ExtractJwt = passportJWT.ExtractJwt;
+const JwtStrategy = passportJWT.Strategy;
+
+const User = require("../models/user");
+
+const jwtOptions = {
+	jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+	secretOrKey: "some_secret"
+};
+
+// Will be called during passport.authenticate
+const verify = async (jwt_payload, done) => {
+	const user = await User.findOne({ _id: jwt_payload.id });
+	if (user) {
+		done(null, user);
+	} else {
+		done(null, false);
+	}
+};
+
+const jwtStrategy = new JwtStrategy(jwtOptions, verify);
+
+passport.use(jwtStrategy);
+
+module.exports = {
+	passport,
+	jwtOptions
+};
