eef295b7a1154be51c700c440d3d80d1b57eb35f
See Loo Jane <muigek34@yahoo.com.sg>
2018-07-17 10:58:24 +0800

    Update username schema for User

--- models/User.js
+++ models/User.js
@@ -1,12 +1,24 @@
 const mongoose = require("mongoose");
+const crypto = require("crypto");
+const uniqueValidator = require("mongoose-unique-validator");
 const ObjectId = mongoose.Schema.Types.ObjectId;
 
 const userSchema = mongoose.Schema({
-	username: { type: String, required: [true, "username is required!"] },
+	username: {
+		type: String,
+		required: [true, "username is required!"],
+		lowercase: true,
+		unique: true,
+		index: true
+	},
 	bio: String,
-    bookmarked: [{type: ObjectId, ref: 'Post'}],
-    likes: [{type: ObjectId, ref: 'Post'}]
+	bookmarked: [{ type: ObjectId, ref: "Post" }],
+	likes: [{ type: ObjectId, ref: "Post" }],
+	hash: String,
+	salt: String
 });
+
+userSchema.plugin(uniqueValidator, { message: "should be unique" });
 
 const user = mongoose.model("User", userSchema);
 
