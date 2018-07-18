fe8a82c5cb2788ddb36aea736193718ca494831b
See Loo Jane <muigek34@yahoo.com.sg>
2018-07-18 10:33:45 +0800

    Add middleware for authorisation

---
+++ middlewares/action-authorisation-checker.js
@@ -0,0 +1,21 @@
+const isUserAuthorisedForAction = (req, res, next) => {
+	if (_isUserAuthorised(req.user._id, req.params.id)) {
+		next();
+	} else {
+		_fireForbiddenError(next)
+	}
+};
+
+const _isUserAuthorised = (authId, reqId) => {
+	return authId.toString() === reqId.toString();
+};
+
+const _fireForbiddenError = (next) => {
+	let error = {
+		name: "ForbiddenError",
+		message: "This action is forbidden for this user!"
+	};
+	next(error);
+};
+
+module.exports = isUserAuthorisedForAction;
