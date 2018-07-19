7a1e55f19845c1465d39f2b4c9095f12d627ab0d
See Loo Jane <muigek34@yahoo.com.sg>
2018-07-19 16:10:15 +0800

    Remove unused code in app.js

--- app.js
+++ app.js
@@ -10,32 +10,13 @@ const postRouter = require("./routes/post-router");
 const userRouter = require("./routes/user-router");
 const bookmarkRouter = require("./routes/bookmark-router");
 
-const Post = require("./models/Post");
-const User = require("./models/User");
 
 // Open DB connection
-
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/cat-gram");
 const db = mongoose.connection;
 db.on("error", error => {
 	console.error("An error occurred!", error);
 });
-
-// const sampleUser = new User({
-// 	username: "Ms Cat",
-// 	bio: "Hello, I meow."
-// });
-
-// sampleUser.save();
-
-// const samplePost = new Post({
-// 	author: "5b4c38193a68d009eb5fb3c0",
-// 	caption: "Im just a cat photo",
-// 	image: "http://sampleurl.com",
-// 	likes: 10
-// });
-
-// samplePost.save();
 
 const app = express();
 
